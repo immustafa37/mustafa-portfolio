64eb2ba43dcecb16682f73185d13ba9baa6a913f
Mustafa Ahsan <im262897tt@gmail.com>
2025-07-17 19:35:59 -0700

    added components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import { BrowserRouter as Router } from 'react-router-dom';
 import Home from './pages/Home';
-import './App.css';
+import './styles.css';
 
 function App() {
   return (
